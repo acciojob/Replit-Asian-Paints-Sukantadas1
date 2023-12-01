9a127b88348b1f7d1a984ee4221e7f6d3c2a0963
Sukanta Das <111263085+Sukantadas1@users.noreply.github.com>
2023-12-01 20:07:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+function changeColor() {
+    const blockIdInput = document.getElementById("block_id");
+    const colorInput = document.getElementById("color_id");
+
+    const blockId = blockIdInput.value;
+    const color = colorInput.value;
+
+    if (blockId < 1 || blockId > 9) {
+        alert("Please enter a valid block ID between 1 and 9.");
+        return;
+    }
+
+    const targetBlock = document.getElementById(blockId.toString());
+    targetBlock.style.backgroundColor = color;
+}
+
+function resetColors() {
+    const gridItems = document.querySelectorAll(".grid-item");
+
+    gridItems.forEach(item => {
+        item.style.backgroundColor = "transparent";
+    });
+}
